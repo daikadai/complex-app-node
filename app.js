6a5654be3d3d3e1713eacdf33dedbad3c553145a
Pham Cao Dai <daikadai1026@gmail.com>
2020-04-04 11:29:19 +0700

    4-What is a model

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const router = require('./router');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
 app.use(express.static('public'));
 app.set('views', 'views');
 app.set('view engine', 'ejs');
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
+const User = require('../models/User');
+
 exports.login = function() {
   
 }
 exports.logout = function() {
-  
+
 }
 exports.register = function(req, res) {
+  let user = new User(req.body);
+  user.register();
   res.send('Thanks for trying to register');
 }
 exports.home = function(req, res) {
