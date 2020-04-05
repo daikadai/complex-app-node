207033e44f25c6051c0d58c11ef40d211230f3a4
Pham Cao Dai <daikadai1026@gmail.com>
2020-04-05 14:48:46 +0700

    12-How can we identify or trust a request

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-const router = require('./router');
-
+const session = require('express-session');
 const app = express();
+
+
+
+let sessionOptions = session({
+  secret: 'JavaScript is sooo cool',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+app.use(sessionOptions);
+
+const router = require('./router');
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
